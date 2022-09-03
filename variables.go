82f6080054d9eb71608f8563ecdbf18aa58fcdae
C-Hag <83981737+C-Hag@users.noreply.github.com>
2022-09-03 19:31:00 +0200

    Update variables.go

--- variables.go
+++ variables.go
@@ -15,10 +15,10 @@ func main() {
 	var sum int
 	fmt.Println("The sum is", sum)
 
-	part1, other := 1, 5
+	part1, other := 2, 5
 	fmt.Println("part 1 is", part1, "other is", other)
 
-	part2, other := 2, 0
+	part2, other := 4, 0
 	fmt.Println("part 2 is", part2, "other is", other)
 
 	sum = part1 + part2
