238d50dd5daac64ae02733cc866acc8c1ed3ec0f
C-Hag <c_hagglund@bahnhof.se>
2022-10-22 22:10:29 +0200

    update

--- webproject/main.go
+++ webproject/main.go
@@ -9,8 +9,13 @@ func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1> Welcome to LinsEYe!</h1>")
 }
 
+//error training
+//err := doStuff()
 func main() {
 	http.HandleFunc("/", HandlerFunc)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
+
+//Debugging example
+//./main.go:11:6: no new variables on left side of :=
