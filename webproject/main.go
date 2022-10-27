a310080370cda15690411a70043f5a22de7eaafe
C-Hag <c_hagglund@bahnhof.se>
2022-10-27 21:18:17 +0200

    Status not found test

--- webproject/main.go
+++ webproject/main.go
@@ -2,17 +2,48 @@ package main
 
 import (
 	"fmt"
-	"os"
-	// "net/http"
+	"net/http"
 )
 
-//func HandlerFunc(w http.ResponseWriter, r *http.Request) {
-//	fmt.Fprint(w, "<h1> Welcome to this cool site!</h1>")
-//}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> Welcome to this future site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in contact, email me at <a href=\"mailto:c_hagglund@bahnhof.se\">c_hagglund@bahnhof.se</a>.</p")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// To handle error messages
+		http.Error(w, "Page not found", http.StatusNotFound)
+
+	}
+}
+
+// If example: localhost:3000/admin generates page not found.
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
 
 func main() {
-	//	http.HandleFunc("/", HandlerFunc)
-	fmt.Fprintln(os.Stdout, "Hello World!")
+	//var router Router
 	fmt.Println("Starting the server on :3000...")
-	// http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 }
