aa244491e8cb8468f6c80a5c8677dcd86f1da3a3
C-Hag <c_hagglund@bahnhof.se>
2022-10-27 07:58:30 +0200

    Adding contact page

--- webproject/main.go
+++ webproject/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
-	"os"
-	// "net/http"
+	"net/http"
 )
 
-//func HandlerFunc(w http.ResponseWriter, r *http.Request) {
-//	fmt.Fprint(w, "<h1> Welcome to this cool site!</h1>")
-//}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1> Welcome to this future site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in contact, email me at <a href=\"mailto:c_hagglund@bahnhof.se\">c_hagglund@bahnhof.se</a>.</p")
+}
 
 func main() {
-	//	http.HandleFunc("/", HandlerFunc)
-	fmt.Fprintln(os.Stdout, "Hello World!")
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
-	// http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", nil)
 }
